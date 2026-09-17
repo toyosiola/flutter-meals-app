cbb5058671b1d79451e12aa2ff022050df60f762
Oluwatoyosi Oladipo <oladipooluwatoyosi@yahoo.com>
2026-09-17 16:03:51 +0100

    Enhance favourite icon with animation on toggle in MealDetailsScreen using implicit animation

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -31,7 +31,19 @@ class MealDetailsScreen extends ConsumerWidget {
                 ),
               );
             },
-            icon: Icon(isFavourite ? Icons.star : Icons.star_border),
+            icon: AnimatedSwitcher(
+              duration: const Duration(milliseconds: 300),
+              transitionBuilder: (child, animation) {
+                return RotationTransition(
+                  turns: Tween<double>(begin: 0.8, end: 1).animate(animation),
+                  child: child,
+                );
+              },
+              child: Icon(
+                isFavourite ? Icons.star : Icons.star_border,
+                key: ValueKey(isFavourite),
+              ),
+            ),
           ),
         ],
       ),
